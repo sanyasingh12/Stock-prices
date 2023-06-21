56d5c0d3c614689cde9f7c38a3dae3ddb969229b
Sanya Singh <130326404+sanyasingh12@users.noreply.github.com>
2023-06-21 23:55:30 +0530

    Add files via upload

---
+++ Stock prices forecasting.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from datetime import datetime
+import tensorflow as tf
+from tensorflow import keras
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+import seaborn as sns
+
+
+# In[2]:
+
+
+#downloading the dataset
+microsoft = pd.read_csv('MicrosoftStock.csv')
+print(microsoft.head())
+
+
+# In[3]:
+
+
+microsoft.info()
+
+
+# In[4]:
+
+
+microsoft.describe()
+
+
+# In[5]:
+
+
+plt.plot(microsoft['date'],
+         microsoft['open'],
+         color="blue",
+         label="open")
+plt.plot(microsoft['date'],
+         microsoft['close'],
+         color="green",
+         label="close")
+plt.title("Microsoft Open-Close Stock")
+plt.legend()
+
+
+# In[6]:
+
+
+plt.plot(microsoft['date'],
+         microsoft['volume'])
+plt.show()
+
+
+# In[7]:
+
+
+sns.heatmap(microsoft.corr(),
+            annot=True,
+            cbar=False)
+plt.show()
+
+
+# In[8]:
+
+
+#plotting close prices of stock
+
+
+# In[9]:
+
+
+microsoft['date'] = pd.to_datetime(microsoft['date'])
+prediction = microsoft.loc[(microsoft['date']
+                            > datetime(2013, 1, 1))
+                            & (microsoft['date']
+                            < datetime(2018, 1, 1))]
+  
+plt.figure(figsize=(10, 10))
+plt.plot(microsoft['date'], microsoft['close'])
+plt.xlabel("Date")
+plt.ylabel("Close")
+plt.title("Microsoft Stock Prices")
+
+
+# In[14]:
+
+
+import math
+
+
+# In[17]:
+
+
+#prepare the training set samples
+msft_close = microsoft.filter(['close'])
+dataset = msft_close.values
+training = int(np.ceil(len(dataset) * 0.95))
+
+
+# In[18]:
+
+
+# scale the data
+ss = StandardScaler()
+ss = ss.fit_transform(dataset)
+
+
+# In[19]:
+
+
+train_data = ss[0:int(training), :]
+  
+x_train = []
+y_train = []
+  
+# considering 60 as the batch size,
+# create the X_train and y_train
+for i in range(60, len(train_data)):
+    x_train.append(train_data[i-60:i, 0])
+    y_train.append(train_data[i, 0])
+  
+x_train, y_train = np.array(x_train),\
+                   np.array(y_train)
+X_train = np.reshape(x_train,
+                     (x_train.shape[0],
+                      x_train.shape[1], 1))
+
+
+# # Building the Model
+
+# In[21]:
+
+
+model = keras.models.Sequential()
+model.add(keras.layers.LSTM(units=64,
+                            return_sequences=True,
+                            input_shape
+                            =(X_train.shape[1], 1)))
+model.add(keras.layers.LSTM(units=64))
+model.add(keras.layers.Dense(128))
+model.add(keras.layers.Dropout(0.5))
+model.add(keras.layers.Dense(1))
+  
+print(model.summary())
+
+
+# # Compiling and Fitting the model
+
+# In[22]:
+
+
+from keras.metrics import RootMeanSquaredError
+model.compile(optimizer='adam',
+              loss='mae',
+              metrics=RootMeanSquaredError())
+  
+history = model.fit(X_train, y_train,
+                    epochs=20)
+
+
+# # Evaluation of Model
+
+# In[23]:
+
+
+testing = ss[training - 60:, :]
+x_test = []
+y_test = dataset[training:, :]
+for i in range(60, len(testing)):
+    x_test.append(testing[i-60:i, 0])
+  
+x_test = np.array(x_test)
+X_test = np.reshape(x_test,
+                    (x_test.shape[0],
+                     x_test.shape[1], 1))
+  
+pred = model.predict(X_test)
+
+
+# # Plotting data and checking trained data
+
+# In[24]:
+
+
+train = microsoft[:training]
+test = microsoft[training:]
+test['Predictions'] = pred
+  
+plt.figure(figsize=(10, 8))
+plt.plot(train['close'], c="b")
+plt.plot(test[['close', 'Predictions']])
+plt.title('Microsoft Stock Close Price')
+plt.ylabel("Close")
+plt.legend(['Train', 'Test', 'Predictions'])
+
